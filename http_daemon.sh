aee1383a72229a1a7ad1047fc0a42bd3c0220bd2
Shmulik Regev (jerry) <shmul@trusteer.com>
2012-06-14 11:32:48 +0300

    changed the lua path

--- http_daemon.sh
+++ http_daemon.sh
@@ -7,6 +7,7 @@ logfile=$shareddir/log/mule_http_daemon.log
 muledir=/home/ranger/mule/current
 muledb=/home/ranger/mule/shared/db/mule.tcb
 hostport=0.0.0.0:8980
+luaexec=/usr/bin/lua
 
 log() {
 	  echo "`date +'%F %T'`: $1 ($$)"
@@ -15,6 +16,6 @@ log() {
 log "Start"
 
 cd $muledir
-/usr/local/bin/lua mule.lua -l $logfile -d $muledb -t $hostport -x nomoremule
+$luaexec mule.lua -l $logfile -d $muledb -t $hostport -x nomoremule
 
 log "End"
