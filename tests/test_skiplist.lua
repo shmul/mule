ae476a2f8487b19103421d859720a4d4a8d962b5
Shmulik Regev (jerry) <shmul@trusteer.com>
2013-01-21 21:00:31 +0200

    added matching_keys test

--- tests/test_skiplist.lua
+++ tests/test_skiplist.lua
@@ -59,3 +59,36 @@ function test_next()
   assert_equal("world",sl:next(f).key)
   assert_nil(sl:next(sl:next(f)))
 end
+
+
+function test_matching_keys()
+  local sl = skip.new()
+  for _,v in ipairs({"hello","cruel","world","crueler","crowd"}) do
+    sl:insert(v)
+  end
+
+  local function matching_keys(prefix_)
+    return coroutine.wrap(
+      function()
+        local find = string.find
+        local node = sl:find(prefix_)
+        -- first node may not match the prefix
+        if node.key<prefix_ then
+          node = sl:next(node)
+        end
+        while node and node.key and find(node.key,prefix_,1,true) do
+          coroutine.yield(node)
+          node = sl:next(node)
+        end
+      end)
+  end
+
+  local fm = {}
+  for i in matching_keys("cru") do
+    table.insert(fm,i.key)
+  end
+
+  assert_equal(2,#fm)
+  assert_equal("cruel",fm[1])
+  assert_equal("crueler",fm[2])
+end
