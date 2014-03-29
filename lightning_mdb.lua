e0ee5acb2530fd7edfc91b5cd3eac8c520865bf0
Shmulik Regev (marathon) <shmul@trusteer.com>
2014-03-29 19:14:29 +0300

    yay. first tests pass

--- lightning_mdb.lua
+++ lightning_mdb.lua
@@ -37,102 +37,122 @@ function chained_db(dbs_,generate_new_db_)
 end
 
 local NUM_PAGES = 25600
-local MAX_DBS = 10000
 
 function lightning_mdb(base_dir_,read_only_,num_pages_)
-  local meta_file = base_dir_.."/db.meta"
-  local _meta = nil
-  local _dbs = {}
-  local _e = nil
+  local _meta,_meta_db
+  local _envs = {}
 
-  local function transaction(func_)
-    local t = _e:txn_begin(nil,0)
+  local function txn(env_,func_)
+    local t = env_:txn_begin(nil,0)
     local rv = func_(t)
     if not rv then
-      t:reset()
+      t:abort()
       return nil
-      end
+    end
     t:commit()
     return rv
   end
 
-  local function new_db(name_)
-    return transaction(
+  local function new_env_factory(name_)
+    local e = lightningmdb.env_create()
+    local full_path = base_dir_.."/"..name_
+    e:set_mapsize((num_pages_ or NUM_PAGES)*4096)
+    os.execute("mkdir -p "..full_path)
+    e:open(full_path,read_only_ and lightningmdb.MDB_RDONLY or 0,420)
+    logi("new_env_factory",full_path)
+    return e
+  end
+
+  local function new_db(env_)
+    return txn(env_,
       function(t)
-        return t:dbi_open(name_,read_only_ and 0 or lightningmdb.MDB_CREATE)
+        local r,err = t:dbi_open(nil,read_only_ and 0 or lightningmdb.MDB_CREATE)
+        if err then
+          loge("new_db",err)
+        end
+        return r
       end)
   end
 
-  local function init()
-    _e = lightningmdb.env_create()
-    _e:set_mapsize((num_pages_ or NUM_PAGES)*4096)
-    _e:set_maxdbs(MAX_DBS)
+  local function add_env()
+    local e = new_env_factory(tostring(#_envs))
+    logi("creating new env pair",#_envs)
+    table.insert(_envs,{e,new_db(e)})
+  end
 
-    _e:open(base_dir_,read_only_ and lightningmdb.MDB_RDONLY or 0,420)
-    _meta = new_db("db.meta")
-    print(_meta)
+  local function init()
+    _meta = new_env_factory("meta")
+    _meta_db = new_db(_meta)
+    add_env()
   end
 
 
   local function put(k,v)
-    return transaction(
-      function(t)
-        if string.find(k,"metadata=",1,true) then
-          return t:put(_meta,k,v,0)
-        end
+    if string.find(k,"metadata=",1,true) then
+      return txn(_meta,function(t) return t:put(_meta_db,k,v,0) end)
+    end
 
-        local rv = t:put(_dbs[#_dbs],k,v)
+    return txn(_envs[#_envs][1],
+      function(t)
+        local rv = t:put(_envs[#_envs][2],k,v)
         if rv then return rv end
-        _dbs[#_dbs+1] = new_db(tostring(#_dbs+1))
-        return t:put(_dbs[#_dbs],k,v)
+        add_env()
+        return put(k,v)
       end)
   end
 
   local function get(k)
-    return transaction(
-      function(t)
-        if string.find(k,"metadata=",1,true) then
-          return t:get(_meta,k)
-        end
-        for _,d in ipairs(_dbs) do
-          local rv = t:get(d,k)
-          if rv then return rv end
-        end
-      end)
+    if string.find(k,"metadata=",1,true) then
+      return txn(_meta,function(t) return t:get(_meta_db,k) end)
+    end
+
+    for _,ed in ipairs(_envs) do
+      local rv = txn(ed[1],function(t) return t:get(ed[2],k) end)
+      if rv then return rv end
+    end
+
   end
 
 
   local function out(k)
-    return transaction(
-      function(t)
-        return each_db(function(d)
-                         if not t:get(d,k) then return false end
-                         t:out(d,k)
-                         return true
-                       end)
-      end)
+    if string.find(k,"metadata=",1,true) then
+      return txn(_meta,function(t) return t:del(_meta_db,k,nil) end)
+    end
+
+    for _,ed in ipairs(_envs) do
+      txn(ed[1],function(t) return t:del(ed[2],k,nil) end)
+    end
   end
 
+  local function matching_keys(prefix_,level_,meta_)
 
-  local function matching_keys(prefix_,level_)
+    local function helper(db)
+      return function(t)
+        local cur = t:cursor_open(db)
+        local k,v = prefix_,nil
+        repeat
+          k,v = cur:get(k,lightningmdb.MDB_NEXT)
+          if k and bounded_by_level(k,prefix_,level_ or 1) then
+            coroutine.yield(k,v)
+          end
+        until not k
+        cur:close()
+             end
+    end
+
+    if meta_ then
+      return coroutine.wrap(function() txn(_meta,helper(_meta_db)) end)
+    end
+
     return coroutine.wrap(
       function()
-        return transaction(
-          function(t)
-            return each_db(function(d)
-                             local cur = t:cursor_open(d)
-                             local k,v = prefix_,nil
-                             repeat
-                               k,v = cursor:get(k,lightningmdb.MDB_NEXT)
-                               if k and bounded_by_level(k,prefix_,level_) then
-                                 coroutine.yield(k,v)
-                               end
-                             until not k
-                             cur:close()
-                           end)
-          end)
+        for _,ed in ipairs(_envs) do
+          txn(ed[1],helper(ed[2]))
+        end
       end)
   end
+
+
 
   local function close()
     _e:close()
--- tests/test_lightning.lua
+++ tests/test_lightning.lua
@@ -16,7 +16,20 @@ function test_meta()
   db.put("metadata=hello","cruel world")
   assert_equal("cruel world",db.get("metadata=hello"))
   assert_nil(db.get("hello"))
+
+  for k,v in db.matching_keys("metadata=hello",1,true) do
+    assert_equal("metadata=hello",k)
+    assert_equal("cruel world",v)
+  end
+  db.out("metadata=hello")
+  assert_nil(db.get("metadata=hello"))
+
+  for k,v in db.matching_keys("metadata=hello",1,true) do
+    -- this will fail
+    assert_not_nil(k)
+  end
+
 end
 
 
-verbose_log(true)
+--verbose_log(true)
