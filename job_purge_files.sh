3ca038379510f46dbc7c49d1abc093deade22e06
shmul <shmul@trusteer.com>
2014-05-10 06:58:50 +0300

    archiving ltmule_processed dir as well

--- job_purge_files.sh
+++ job_purge_files.sh
@@ -2,8 +2,6 @@
 
 basedir=/home/trusteer/mule
 shareddir=$basedir/shared
-processed=$shareddir/queues/mule_processed
-
 
 function log() {
   echo "`date --iso-8601=seconds` ($$) $*"
@@ -14,14 +12,19 @@ log "start"
 for i in `seq 4 15`; do
   dir=`date --date="$i day ago" +'%y/%m/%d'`
   archive=`date --date="$i day ago" +'%y.%m.%d'`
-  fulldir="$processed/$dir"
-  log "$processed/$archive.tgz from $fulldir"
-  if [[ -d $fulldir ]]; then
-    tar zcf $processed/$archive.tgz -C $processed $dir --remove-files
-  fi
+
+  for j in $shareddir/queues/mule_processed  $shareddir/queues/ltmule_processed; do
+    fulldir="$j/$dir"
+    log "$j/$archive.tgz from $fulldir"
+    if [[ -d $fulldir ]]; then
+      tar zcf $j/$archive.tgz -C $j $dir --remove-files
+    fi
+  done
+
 done
 
 log "erasing old mule logs"
 find $shareddir/ -name "mule.log-*" -mtime +30 -delete
+find $shareddir/ -name "ltmule.log-*" -mtime +30 -delete
 
 log "end"
