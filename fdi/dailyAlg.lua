93e744e0cead0adfbb9313217c57e00ce9f544ae
shmul <shmul@trusteer.com>
2014-10-13 10:27:06 +0300

    changes from Ehud

--- fdi/dailyAlg.lua
+++ fdi/dailyAlg.lua
@@ -1,7 +1,8 @@
 ------------------------------------
--- Daily algo, Matlab version 32 ---
+-- Daily algo, Matlab version 33 ---
 ------------------------------------
 require 'helpers'
+require 'statisticsBasic'
 
 -- costants
 local INTERVAL = 86400
@@ -16,14 +17,13 @@ local LOGNORMAL_SHIFT = 100
 local MAX_ALARM_PERIOD = 4
 local R_DRIFT = 1.5
 local R_SAFETY = 2
-local MAX_SD_CHANGE = 0.017
+local SD_EST_PERIOD = 28
 local MIN_SD = 0.1
 
 -- initial model
 local INIT_SD = 0.5
 local INIT_R = 0.2
 local INIT_M = math.log(LOGNORMAL_SHIFT)
-
 
 function calculate_fdi_days(times_, values_)
 
@@ -35,31 +35,20 @@ function calculate_fdi_days(times_, values_)
   -- change model
   local upperCusum = 0
   local lowerCusum = 0
-  local weeklyWindow = {INIT_M, INIT_M, INIT_M, INIT_M, INIT_M, INIT_M, INIT_M}
+  local weeklyWindow = {}
+	local devWindow = {}
   local alarmPeriod = 0
   local rPeriod = 0
-  local insert = table.insert
+	local lastRef = INIT_M
 
-  local function median(array_)
-    local sorted = {}
-    for _,value in pairs(array_) do
-      insert(sorted, value)
-    end
-    table.sort(sorted)
-    local res
-    local len = #sorted
-		if len == 0 then
-				loge('median - array size must be greater than zero')
-				return 0
-		end
-    if (len % 2 == 0) then
-      local i = len/2
-      res = (sorted[i] + sorted[i+1] ) / 2
-    else
-      res = sorted[math.ceil(len/2)]
-    end
-    return res
-  end
+	-- downtime
+	local downtime = 0
+  local lastDowntime = 0
+
+	-- lua optimization
+  local insert = table.insert
+	local remove = table.remove
+
 
   local function iter(timestamp_, value_)
 
@@ -71,6 +60,13 @@ function calculate_fdi_days(times_, values_)
       y = tval + r
     else
       y = tval
+    end
+
+		if(value_ == 0) then
+		  downtime = downtime + 1
+      lastDowntime = ii
+    else
+      downtime = 0
     end
 
     if(alarmPeriod < MAX_ALARM_PERIOD) then
@@ -91,21 +87,6 @@ function calculate_fdi_days(times_, values_)
 
         -- update m
         m = m + FORGETTING_FACTOR * err
-
-        -- update sd
-        local sdtemp = math.sqrt((1-FORGETTING_FACTOR)*sd^2 + FORGETTING_FACTOR*err^2)
-        if(sdtemp - sd > MAX_SD_CHANGE) then
-          sd = sd + MAX_SD_CHANGE
-        elseif(sdtemp - sd < -MAX_SD_CHANGE) then
-          sd = sd - MAX_SD_CHANGE
-        else
-          sd = sdtemp
-        end
-        if(sd < MIN_SD) then
-          sd = MIN_SD
-        end
-
-
       end
 
     else -- (alarmPeriod == MAX_ALARM_PERIOD)
@@ -114,55 +95,124 @@ function calculate_fdi_days(times_, values_)
       alarmPeriod = 0
 
       m = y
+
+			lastRef = m;
+		  for jj = 1,MAX_ALARM_PERIOD-1 do
+        devWindow[SD_EST_PERIOD + 1 - jj] = weeklyWindow[WEEK + 1 - jj] - lastRef;
+      end
+
+    end
+
+		-- update sd
+		if(downtime == 0) then
+		  remove(devWindow, 1)
+		  insert(devWindow, y-lastRef)
+      if(alarmPeriod == 0) then
+				local devInd = {}
+				local trDevWindow = {}
+				for ii = 1,SD_EST_PERIOD do
+		      devInd[ii] = true
+					trDevWindow[ii] = devWindow[ii]
+        end
+
+        while true do
+            local stop = true
+            local mu = mean(trDevWindow)
+            local sig = std(trDevWindow)
+            if(sig == 0) then
+                break
+            end
+						for ii,value in pairs(devWindow) do
+								if(devInd[ii] and (math.abs((value - mu)/sig) >= 2)) then
+								  devInd[ii] = false
+									stop = false
+								end
+						end
+						trDevWindow = {}
+            for ii = 1,SD_EST_PERIOD do
+				      if(devInd[ii]) then
+                insert(trDevWindow, devWindow[ii])
+				      end
+            end
+            if(stop) then
+                break
+            end
+        end
+
+
+        local sdx = std(trDevWindow) * SD_EST_PERIOD / #trDevWindow;
+        sd = math.sqrt((1-FORGETTING_FACTOR)*sd^2 + FORGETTING_FACTOR*sdx^2);
+        if(sd < MIN_SD) then
+            sd = MIN_SD
+        end
+      end
     end
 
     -- update r
-    table.remove(weeklyWindow, 1)
+    remove(weeklyWindow, 1)
     insert(weeklyWindow, tval)
 
-    local mwa = {}
-    local mna = {}
-    for jj,v in ipairs(weeklyWindow) do
-      if((((ii-WEEK)+jj) % 7) < 2) then
-        insert(mwa,v)
-      else
-        insert(mna, v)
-      end
-    end
-    local mout = median(mna)
-    local nout = median(mwa)
-
-    local wdif = 0
-    for _,value in pairs(mwa) do
-      if(math.abs(value - nout) > R_SAFETY*sd) then wdif = wdif + 1 end
-    end
-    local ndif = 0
-    for _,value in pairs(mna) do
-      if(math.abs(value - mout) > R_SAFETY*sd) then ndif = ndif + 1 end
-    end
-
-    if((wdif == 0) and (ndif == 0) and (mout >= nout)) then
-      local rout = mout - nout
-
-      if(rPeriod < MAX_ALARM_PERIOD) then
-        if((math.abs(rout - r) > R_DRIFT)) then
-          rPeriod = rPeriod + 1
+    if(ii - lastDowntime > WEEK) then
+      local mwa = {}
+      local mna = {}
+      for jj,v in ipairs(weeklyWindow) do
+        if((((ii-WEEK)+jj) % 7) < 2) then
+          insert(mwa,v)
         else
-          r = r + FORGETTING_FACTOR * (rout - r)
+          insert(mna, v)
+        end
+      end
+      local mout = median(mna)
+      local nout = median(mwa)
+
+      local wdif = 0
+      for _,value in pairs(mwa) do
+        if(math.abs(value - nout) > R_SAFETY*sd) then wdif = wdif + 1 end
+      end
+      local ndif = 0
+      for _,value in pairs(mna) do
+        if(math.abs(value - mout) > R_SAFETY*sd) then ndif = ndif + 1 end
+      end
+
+      if((wdif == 0) and (ndif == 0) and (mout >= nout)) then
+        local rout = mout - nout
+
+        if(rPeriod < MAX_ALARM_PERIOD) then
+          if((math.abs(rout - r) > R_DRIFT)) then
+            rPeriod = rPeriod + 1
+          else
+            r = r + FORGETTING_FACTOR * (rout - r)
+            rPeriod = 0
+          end
+        else
+          r = rout
           rPeriod = 0
         end
       else
-        r = rout
         rPeriod = 0
       end
-    else
-      rPeriod = 0
-    end
 
+		else
+      if(downtime > WEEK) then
+        r = 0
+        rPeriod = 0
+      end
+    end
 
     return (alarmPeriod > 0)
   end
 
+
+
+	-- initialize
+	for ii = 1,WEEK do
+		weeklyWindow[ii] = INIT_M
+  end
+
+	local initDev = INIT_SD * math.sqrt(27/28)
+	for ii = 1,SD_EST_PERIOD do
+		devWindow[ii] = initDev * ((-1)^ii)
+  end
 
   local result = {}
 
@@ -175,7 +225,7 @@ function calculate_fdi_days(times_, values_)
   -- detect changes
   for ii=1,range do
     local alert = iter(times_[ii], values_[ii])
-    local x = {times_[ii], alert}
+		local x = {times_[ii], alert}
     insert(result, x)
   end
 
