606d5f227c8a37610d329895236fa6af1896d352
shmul <shmul@il.ibm.com>
2015-02-13 19:08:42 +0200

    now with static charts

--- scent/app.js
+++ scent/app.js
@@ -1,4 +1,51 @@
 
 function app() {
+  function load_graph(graph_,target_) {
+    var graph = scent_ds.graph(graph_);
+    var raw_data = graph[1][graph[0]];
+    var data = [];
+    for (var rw in raw_data) {
+      var dt = raw_data[rw][2];
+      if ( dt>100000 ) {
+        data.push({date:new Date(dt*1000),value:raw_data[rw][0]});
+      }
+    };
+    data.sort(function(a,b) { return a.date-b.date });
+
+
+    var markers = [{
+      'date': new Date('2014-05-01T00:00:00.000Z'),
+      'label': 'Anomaly'
+    }];
+
+    MG.data_graphic({
+      title: graph[0],
+      data: data,
+      markers: markers,
+      show_secondary_x_label: false,
+      //markers: [{'year': 1964, 'label': '"The Creeping Terror" released'}],
+      full_width: true,
+      full_height: true,
+      target: target_,
+      //x_accessor: "date-time",
+      //y_accessor: "sightings",
+      //interpolate: "monotone",
+      missing_is_zero: true,
+    });
+  }
+
+  load_graph("httpreq","#chart-1");
+  load_graph("httpreq","#chart-2");
+  load_graph("httpreq","#chart-3");
+  /*
+    $(function() {
+    $('#chart-1').on('click', function() {
+    alert("chart-1");
+    });
+    });
+  */
 
 }
+
+
+$(app);
--- scent/datasource.js
+++ scent/datasource.js
@@ -1,9 +1,23 @@
 // all data items on the page come from here. We can work in mockup or real world mode
 
-function scent_ds_mockup () {
-  var fixtures = ["s3.phishing","httpreq","alerts"];
-  for (var f in fixtures) {
-    jQuery.getScript("./fixtures/"+fixtures[f]+".json");
+function scent_ds_mockup (ready_) {
+  var fixtures_scripts = ["s3.phishing","httpreq","alerts"];
+  var fixtures = {};
+
+  function load_fixture(name_,path_) {
+    $.ajax({
+      url: "./fixtures/"+name_+".json",
+      async: false,
+      dataType: 'json',
+      success: function (response_) {
+        fixtures[name_] = response_.data;
+      }
+    });
+  }
+
+
+  for (var f in fixtures_scripts) {
+    load_fixture(fixtures_scripts[f]);
   }
 
   function alerts(alert_type_) {
@@ -13,12 +27,22 @@ function scent_ds_mockup () {
   }
 
   function graph(graph_name_) {
-
+    var gr = fixtures[graph_name_];
+    if ( gr["httpreq.frontend_messaging.304;1h:90d"] ) {
+      return ["httpreq.frontend_messaging.304;1h:90d",gr];
+    }
+    if ( gr["download_from_s3.downloads.phishing_constant_patterns;1h:90d"] ) {
+      return ["download_from_s3.downloads.phishing_constant_patterns;1h:90d",gr];
+    }
   }
 
   return {
     critical : function() { return alerts("critical"); },
+    graph : graph,
   }
 
 
 };
+
+
+scent_ds = scent_ds_mockup();
