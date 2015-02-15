5fb98ea36c18c7faa13001a6bc60505e7f329859
Dan Carmon <dan.carmon@trusteer.com>
2015-02-15 14:06:37 +0200

    Removed Ruby from production vagrant setup script

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -20,21 +20,6 @@ sudo luarocks install copas 1.1.6-1
 sudo luarocks install lpack
 sudo luarocks install luaposix
 
-if ! which rvm
-then
-  : Installing RVM, Ruby:
-  : ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  curl -L https://get.rvm.io | HOME=/home/vagrant bash -ls stable --ruby=1.9.3 --verify-downloads 1
-fi
-
-: Setting up Nginx
-: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-source_nginx_conf="/vagrant/mule_nginx.conf"
-target_nginx_conf="/etc/nginx/sites-enabled/mule_nginx.conf"
-
-if ! [ -L $target_nginx_conf ]
-then
-  sudo ln -s $source_nginx_conf $target_nginx_conf
-fi
-sudo service nginx restart
+# TODO:
+# - Add upstart / system[d,x] configuraiton for mule
+# - Apply nginx configuration
