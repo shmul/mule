ddd999747de64fc5b77778b6545b021dc3ff3106
shmul <shmul@il.ibm.com>
2015-02-17 11:45:37 +0200

    added compilation of lightningdbm and mdb

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -12,13 +12,25 @@ if ! which curl
 then
   sudo apt-get update -y
 fi
-sudo apt-get install -y lua5.1 luarocks nginx
+sudo apt-get install -y lua5.1 luarocks nginx git
+
+: Installing Lightningmdb:
+: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+mkdir installs
+pushd installs
+git clone https://gitorious.org/mdb/mdb.git
+cd mdb/libraries/liblmdb/
+make
+sudo make install
+sudo ldconfig # we need to rebuild the cache to have libmdb.so discoverable
+popd
 
 : Installing rocks:
 : ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sudo luarocks install copas 1.1.6-1
 sudo luarocks install lpack
 sudo luarocks install luaposix
+sudo luarocks install lightningmdb
 
 # TODO:
 # - Add upstart / system[d,x] configuraiton for mule
