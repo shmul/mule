39f5d43a8ac9edbf517f684440f1cc2003191407
shmul <shmul@il.ibm.com>
2015-02-26 17:40:05 +0200

    added a real world mule connectivity

--- scent/datasource.js
+++ scent/datasource.js
@@ -1,6 +1,6 @@
 // all data items on the page come from here. We can work in mockup or real world mode
 
-function scent_ds_mockup (ready_) {
+function scent_ds_mockup () {
   var fixtures_scripts = ["config","key","graph","alert"];
   var fixtures = {};
   const user = "Shmul the mule";
@@ -36,7 +36,7 @@ function scent_ds_mockup (ready_) {
   }
 
   function delayed(func_) {
-    $.doTimeout(100,func_);
+    $.doTimeout(2,func_);
   }
 
   function config(callback_) {
@@ -109,7 +109,6 @@ function scent_ds_mockup (ready_) {
   }
 
   return {
-    critical : function() { return alerts("critical"); },
     config : config,
     graph : graph,
     key : key,
@@ -121,5 +120,85 @@ function scent_ds_mockup (ready_) {
 
 };
 
+function scent_ds() {
+  var ns = $;//$.initNamespaceStorage('mule: ');
+  const user = "Shmul the mule";
+  const mule_server = "http://v2.mule.trusteer.net/mule/api";
 
-scent_ds = scent_ds_mockup();
+  function delayed(func_) {
+    $.doTimeout(2,func_);
+  }
+
+  function mule_get(resource_,callback_) {
+    $.ajax({
+      url: mule_server+resource_,
+      async: true,
+      dataType: 'json',
+      success: function (response_) {
+        callback_(response_.data);
+      },
+      error: function(xhr_,internal_error_,http_status_) {
+        console.log("%s <%s,%s>",resource_,internal_error_,http_status_);
+      }
+    });
+  }
+
+  function config(callback_) {
+    mule_get("/config",callback_);
+  }
+
+  function graph(graph_,callback_) {
+    mule_get("/graph/"+graph_,function(data_) {
+      callback_(data_[graph_]);
+    });
+  }
+
+  function key(key_,callback_) {
+    mule_get("/key/"+key_+"?level=1",callback_);
+  }
+
+  function alerts(callback_) {
+    mule_get("/alert",callback_);
+  }
+
+  // if key == "persistent" the data is taken from the server, otherwise session storage
+  // is used
+  function load(user_,key_,callback_) {
+    delayed(function() {
+      if (key_=="persistent" ) {
+        callback_(ns.localStorage.get(user_+".persistent"));
+      } else {
+        callback_(ns.sessionStorage.get(user_+"."+key_));
+      }
+    });
+  }
+
+  function save(user_,key_,data_,callback_) {
+    delayed(function() {
+      if (key_=="persistent" ) {
+        ns.localStorage.set(user_+".persistent",data_);
+        if ( callback_ ) {
+          callback_();
+        }
+        return;
+      }
+      ns.sessionStorage.set(user_+"."+key_,data_);
+      if ( callback_ ) {
+        callback_();
+      }
+    });
+  }
+
+  return {
+    config : config,
+    graph : graph,
+    key : key,
+    alerts : alerts,
+    load: load,
+    save: save,
+  }
+
+}
+
+//scent_ds = scent_ds_mockup();
+scent_ds = scent_ds();
