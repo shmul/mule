7635053728f6a3cc5a6256adf6c2ebac356a40c4
Dov Murik <dmurik@us.ibm.com>
2015-07-08 21:22:48 -0400

    support testing in docker containers for various Lua versions
    To run the tests on a given platform (Lua version, or combination of
    rocks/libraries):
    
        docker/build_container_and_run_tests.sh lua5.2

---
+++ docker/build_container_and_run_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Usage: docker/build_container_and_run_tests.sh lua5.2
+#
+
+platform=$1
+if [ -z "$platform" ] ; then
+	echo "Usage: $0 <platform_name>"
+	echo ""
+	echo "Example: $0 lua5.2"
+	exit 1
+fi
+
+# Docker repo name
+repo=mule
+
+projectrootrelative="$(dirname $0)/.."
+projectroot=$(cd $mulerootrelative && pwd) # Get the absolute path
+dockerfile="$projectroot/docker/Dockerfile-$platform"
+
+if [ ! -f "$dockerfile" ] ; then
+	echo "Missing docker file: $dockerfile"
+	exit 2
+fi
+
+containername="muletest_$platform"
+docker build --file=$dockerfile --tag=$repo:$platform $projectroot
+docker run --name=$containername $repo:$platform
+retval=$?
+docker rm $containername
+
+exit $retval
