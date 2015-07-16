d525d1abee92c9fabb5456c2a9f34e45aeb5ecb7
shmul <shmul@il.ibm.com>
2015-07-16 11:51:41 +0300

    docker builder typo fixed. added ignored directories

--- docker/build_container_and_run_tests.sh
+++ docker/build_container_and_run_tests.sh
@@ -6,26 +6,26 @@
 
 platform=$1
 if [ -z "$platform" ] ; then
-	echo "Usage: $0 <platform_name>"
+	echo "Usage: $0 <platform_name> [docker build params]"
 	echo ""
 	echo "Example: $0 lua5.2"
 	exit 1
 fi
 
+shift
 # Docker repo name
 repo=mule
 
 projectrootrelative="$(dirname $0)/.."
-projectroot=$(cd $mulerootrelative && pwd) # Get the absolute path
+projectroot=$(cd $projectrootrelative && pwd) # Get the absolute path
 dockerfile="$projectroot/docker/Dockerfile-$platform"
-
 if [ ! -f "$dockerfile" ] ; then
 	echo "Missing docker file: $dockerfile"
 	exit 2
 fi
 
 containername="muletest_$platform"
-docker build --file=$dockerfile --tag=$repo:$platform $projectroot
+docker build --file=$dockerfile --tag=$repo:$platform $@ $projectroot
 docker run --name=$containername $repo:$platform
 retval=$?
 docker rm $containername
