b0ed263102500ab4956894d46ec7b135f54f18bb
Shmulik Regev <shmul@users.noreply.github.com>
2016-05-09 09:57:40 +0300

    Delete private file

--- watcher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-while true; do printf "%s %3s %3s\n" `date +"%H:%M:%S"` `find ~/mule/shared/queues/ltmule_incoming/   | wc -l` `tail -50 ~/mule/shared/logs/ltmule.log | grep -c accepting` ; sleep 2;done
